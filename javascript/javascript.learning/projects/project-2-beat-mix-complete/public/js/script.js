89255e7c06156cbc88e11da79fed11467931309a
Matthew Griffes <mgriffes@gmail.com>
2018-11-24 12:42:45 -0800

    Today's work

--- javascript/javascript.learning/projects/project-2-beat-mix-complete/public/js/script.js
+++ javascript/javascript.learning/projects/project-2-beat-mix-complete/public/js/script.js
@@ -17,7 +17,7 @@ const getDrumArrayByName = (name) => {
     case 'rideCymbals':
       return rideCymbals;
     default:
-    return;
+      return;
   }
 };
 
@@ -31,10 +31,33 @@ const toggleDrum = (drumArrayName, index) => {
 
 const clear = (drumArrayName) => {
   const drums = getDrumArrayByName(drumArrayName);
+  if (drums) {
+    drums.fill(false);
+  }
+};
+
+const invert = (drumArrayName) => {
+  const drums = getDrumArrayByName(drumArrayName);
   if (!drums) {
     return;
   }
-  for let i = 0; i < drums.length; i++) {
+  for (let i = 0; i < drums.length; i++) {
     drums[i] = !drums[i];
   }
+};
+
+const getNeighborPads = (x, y, size) => {
+  const neighborPads = [];
+  if (x >= size || y >= size || x < 0 || y < 0 || size < 1) {
+    return neighborPads;
+  }
+  neighborPads.push([x - 1, y]);
+  neighborPads.push([x, y - 1]);
+  neighborPads.push([x + 1, y]);
+  neighborPads.push([x, y + 1]);
+  return neighborPads.filter((neighbor) => {
+    return neighbor.every((val) => {
+      return val >= 0 && val < size;
+    });
+  });
 };
